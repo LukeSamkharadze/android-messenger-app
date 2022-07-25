0d44874ac6064c2d0d5adbf34e2cddac4c4ebcfb
lukesamkharadze <lukesamkharadze@gmail.com>
2022-07-25 20:12:03 +0400

    init

---
+++ app/src/main/java/com/freeuni/messenger_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.freeuni.messenger_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
