720cf8a7762f34fd2fe3b783eaf9bf49862a11c9
lukesamkharadze <lukesamkharadze@gmail.com>
2022-07-27 10:15:40 +0400

    start auth activity from main activity

--- app/src/main/java/com/freeuni/messenger_app/MainActivity.kt
+++ app/src/main/java/com/freeuni/messenger_app/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.freeuni.messenger_app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.freeuni.messenger_app.activities.AuthActivity
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_auth)
+
+    finish()
+    startActivity(Intent(this, AuthActivity::class.java))
   }
 }
